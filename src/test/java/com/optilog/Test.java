e20c1a2e4ff2ae575be9ca7a7bc699370eb25cfd
Opti <email@example.com>
2022-05-21 21:47:50 +0800

    2022.5.1-Client(add AnnotationProcessor)

--- src/test/java/com/optilog/Test.java
+++ src/test/java/com/optilog/Test.java
@@ -1,21 +1,11 @@
 package com.optilog;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import com.optilog.log.Log;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        File file = new File("D://test.log");
-        if (!file.isFile()) {
-            if (!file.createNewFile()) {
-                throw new RuntimeException();
-            }
-        }
-        
-        String s = Files.readString(Path.of(file.getPath()), StandardCharsets.UTF_8);
-        Files.writeString(Path.of(file.getPath()), s + "hello,world", StandardCharsets.UTF_8);
+    public static final Log log = Log.initLog("");
+    
+    public static void main(String[] args) {
+    
     }
 }
