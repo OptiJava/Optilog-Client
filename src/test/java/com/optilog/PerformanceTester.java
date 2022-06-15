9aae809cfa90649d16b692bacf968c7abb642fdb
OptiJava <OptiJava@protonmail.com>
2022-06-15 15:33:48 +0800

    2022.6.4-Client

--- src/test/java/com/optilog/PerformanceTester.java
+++ src/test/java/com/optilog/PerformanceTester.java
@@ -4,10 +4,7 @@ import com.optilog.log.Log;
 
 public class PerformanceTester {
     public static void main(String[] args) {
-        Log log = Log.initLog("%prop -cp /Settings.properties");
-
-        long l = System.currentTimeMillis();
-        log.info("");
-        System.out.println(System.currentTimeMillis() - l);
+        Log log = Log.initLog("");
+        log.info();
     }
 }
