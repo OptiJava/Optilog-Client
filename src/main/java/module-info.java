b3e00a8e32a203d6d253adf34aeee5818aa8d463
OptiJava <OptiJava@protonmail.com>
2022-06-17 09:39:04 +0800

    2022.6.4-Client(edit module-info.java)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module com.optilog {
+    exports com.optilog.log;
+    exports com.optilog.annotation;
+    exports com.optilog.util;
+
     requires java.sql;
     requires com.google.gson;
     requires com.fasterxml.jackson.dataformat.xml;
