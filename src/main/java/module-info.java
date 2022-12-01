52b8bc804ee118d38e77cca7d4170bfbc2afeb1b
OptiJava <OptiJava@protonmail.com>
2022-12-01 18:54:21 +0800

    2022.6.4-Client(update module-info.java)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module com.optilog {
     exports com.optilog.log;
-    exports com.optilog.annotation;
     exports com.optilog.util;
     exports com.optilog.log.jdbc;
     exports com.optilog.setting;
